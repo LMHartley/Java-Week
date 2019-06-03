383b5a7630d22bb53ebdd0310237562afc6639a5
LMHartley <liamhartley321@live.co.uk>
2019-06-03 11:42:37 +0100

    03/06/2019 11:42

---
+++ For.java
@@ -0,0 +1,8 @@
+class For{
+    public static void main(String XYZ[]){
+        int A,B,C;
+        for(A=1,B=10,C=100;A<=10;A++,B+=10,C+=100){
+            System.out.println(A+"/"+B+"/"+C);
+        }
+    }
+}
