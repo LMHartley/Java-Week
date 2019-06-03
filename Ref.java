ac21ed0300af438969f010765b2413d8ef2907c0
LMHartley <liamhartley321@live.co.uk>
2019-06-03 12:52:21 +0100

    03/06/2019 12:52

---
+++ Ref.java
@@ -0,0 +1,13 @@
+class First{
+    int x;
+    int y;
+}
+class Ref{
+    public static void main(String XYZ[]){
+        First Ref;
+    Ref=new First();
+    Ref.x=15;
+    Ref.y=20;
+    System.out.println(Ref.x+Ref.y);
+    }
+}
---
+++ Test2.java
@@ -0,0 +1,15 @@
+class Second{
+    int Phy,Che;
+    public void message(){
+        System.out.println("Hello");
+    }
+}
+
+class Test2{
+    public static void main(String XZY[]){
+        Second x;
+        x=new Second();
+        x.Phy=75;
+        x.message();
+    }
+}
